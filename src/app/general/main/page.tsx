eca0ca515e48f85fcb2bd581097260b5a7971894
Tomasz Wirkus <wirkus919@gmail.com>
2025-02-27 14:26:52 +0100

    Made sure to correctly load data back

--- src/app/general/main/page.tsx
+++ src/app/general/main/page.tsx
@@ -1,13 +1,47 @@
 import { StatsTableContainer } from "@/components/grids/main/StatsTableContainer";
 import { prisma } from "@/lib/prisma";
+import { NUMBER_OF_MATCHES_SEPARATOR, SEPARATOR_PLAYER } from "@/utils";
 import { defaultOrderBy } from "@/utils/order";
 
 const getGeneralPlayers = async () => {
-  const generalPlayers = await prisma.sessionPlayerStats.findMany({
+  const playersWithMoreGamesThanSeparator = await prisma.playerStats.findMany({
+    where: {
+      isSessionPlayer: false,
+      gamesPlayed: {
+        gte: NUMBER_OF_MATCHES_SEPARATOR,
+      },
+    },
     orderBy: defaultOrderBy,
   });
 
-  return generalPlayers;
+  const playersWithLessGamesThanSeparator = await prisma.playerStats.findMany({
+    where: {
+      isSessionPlayer: false,
+      gamesPlayed: {
+        lt: NUMBER_OF_MATCHES_SEPARATOR,
+      },
+    },
+    include: {
+      weapons: true,
+    },
+    orderBy: [
+      {
+        gamesPlayed: "desc",
+      },
+      {
+        winRatePercentage: "desc",
+      },
+      {
+        kda: "desc",
+      },
+    ],
+  });
+
+  return [
+    ...playersWithMoreGamesThanSeparator,
+    SEPARATOR_PLAYER,
+    ...playersWithLessGamesThanSeparator,
+  ];
 };
 
 export default async function GeneralPage() {
